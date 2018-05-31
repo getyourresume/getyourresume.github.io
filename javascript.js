1310a17378217c9f8c2eadb15b057284d649a2d7
Pranjal_rawat <34087626+PranjalRawat@users.noreply.github.com>
2018-05-31 21:09:54 +0530

    first

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function searchFunction(){
+    var input, filter, ul, li, a, i;
+    input=document.getElementById("myinputsearch");
+    filter=input.value.toLowerCase();
+    alert(filter);
+chrome.tabs.create({url:"https://google.com"});}
