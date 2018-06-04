e20b72d481809443113241073fabffbc53c627c1
Pranjal_rawat <34087626+PranjalRawat@users.noreply.github.com>
2018-06-04 16:07:17 +0530

    A

--- javascript.js
+++ javascript.js
@@ -2,3 +2,10 @@ window.addEventListener("load", function(){
     var load_screen = document.getElementById("load_screen");
     document.body.removeChild(load_screen);
 });
+
+function searchFunction(url){
+    var input,filter;
+    input = document.getElementById('myinputsearch');
+    filter = input.value.toLowerCase();
+
+}
