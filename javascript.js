ab86c7a13f3e0dd48fbb1c6c22643a5b1e359d36
Pranjal_rawat <34087626+PranjalRawat@users.noreply.github.com>
2018-06-04 21:42:40 +0530

    A

--- javascript.js
+++ javascript.js
@@ -3,9 +3,43 @@ window.addEventListener("load", function(){
     document.body.removeChild(load_screen);
 });
 
-function searchFunction(url){
-    var input,filter;
-    input = document.getElementById('myinputsearch');
-    filter = input.value.toLowerCase();
-
-}
+function searchFunction() {
+            var a = document.getElementById('myinputsearch');
+            var e = document.getElementById('click');
+            var c = a.value.toLowerCase();
+            if (c === 'bca') {
+                e.href = 'https://getyourresume.github.io/fresher/bca.html';
+                e.click();
+            }
+            if (c === 'bscit') {
+                e.href = 'https://getyourresume.github.io/fresher/bscit.html';
+                e.click();
+            }
+            if (c === 'bba') {
+                e.href = 'https://getyourresume.github.io/fresher/bba.html';
+                e.click();
+            }
+            if (c === 'bhm') {
+                e.href = 'https://getyourresume.github.io/fresher/bhm.html';
+                e.click();
+            }
+            if (c === 'mca') {
+                e.href = 'https://getyourresume.github.io/fresher/mca.html';
+                e.click();
+            }
+            if (c === 'mscit') {
+                e.href = 'https://getyourresume.github.io/fresher/mscit.html';
+                e.click();
+            }
+            if (c === 'mba') {
+                e.href = 'https://getyourresume.github.io/fresher/mba.html';
+                e.click();
+            }
+            if (c === 'chm') {
+                e.href = 'https://getyourresume.github.io/fresher/chm.html';
+                e.click();
+            }
+            else {
+                alert('No Search Result ');
+            }
+        }
