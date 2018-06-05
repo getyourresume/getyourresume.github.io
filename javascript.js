061436a8a3d54c3f2d7506220bdb39efce34e82f
Pranjal_rawat <34087626+PranjalRawat@users.noreply.github.com>
2018-06-05 16:21:44 +0530

    A

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", function(){
-    var load_screen = document.getElementById("load_screen");
-    document.body.removeChild(load_screen);
+window.addEventListener("load",function(){
+    var preload = document.getElementById("preload");
+    document.body.removeChild(preload);
 });
-
+                       
 function searchFunction() {
             var a = document.getElementById('myinputsearch');
             var e = document.getElementById('click');
@@ -39,7 +39,44 @@ function searchFunction() {
                 e.href = 'https://getyourresume.github.io/fresher/chm.html';
                 e.click();
             }
-            else {
-                alert('No Search Result ');
-            }
+            
         }
+
+function searchFunctionexp() {
+    var a = document.getElementById('myinputsearch');
+            var e = document.getElementById('click');
+            var c = a.value.toLowerCase();
+            if (c === 'bca') {
+                e.href = 'https://getyourresume.github.io/expericenced/bca.html';
+                e.click();
+            }
+            if (c === 'bscit') {
+                e.href = 'https://getyourresume.github.io/expericenced/bscit.html';
+                e.click();
+            }
+            if (c === 'bba') {
+                e.href = 'https://getyourresume.github.io/expericenced/bba.html';
+                e.click();
+            }
+            if (c === 'bhm') {
+                e.href = 'https://getyourresume.github.io/expericenced/bhm.html';
+                e.click();
+            }
+            if (c === 'mca') {
+                e.href = 'https://getyourresume.github.io/expericenced/mca.html';
+                e.click();
+            }
+            if (c === 'mscit') {
+                e.href = 'https://getyourresume.github.io/expericenced/mscit.html';
+                e.click();
+            }
+            if (c === 'mba') {
+                e.href = 'https://getyourresume.github.io/expericenced/mba.html';
+                e.click();
+            }
+            if (c === 'chm') {
+                e.href = 'https://getyourresume.github.io/expericenced/chm.html';
+                e.click();
+            }
+           
+}
