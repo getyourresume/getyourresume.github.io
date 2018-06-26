131a5797b602c0f9783dc8ce8857e5a16f022e56
getyourresume <39804137+getyourresume@users.noreply.github.com>
2018-06-26 09:23:57 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,80 +3,25 @@ window.addEventListener("load",function(){
     document.body.removeChild(preload);
 });
                        
-function searchFunction() {
-            var a = document.getElementById('myinputsearch');
-            var e = document.getElementById('click');
-            var c = a.value.toLowerCase();
-            if (c === 'bca') {
-                e.href = 'https://getyourresume.github.io/fresher/bca.html';
-                e.click();
-            }
-            if (c === 'bscit') {
-                e.href = 'https://getyourresume.github.io/fresher/bscit.html';
-                e.click();
-            }
-            if (c === 'bba') {
-                e.href = 'https://getyourresume.github.io/fresher/bba.html';
-                e.click();
-            }
-            if (c === 'bhm') {
-                e.href = 'https://getyourresume.github.io/fresher/bhm.html';
-                e.click();
-            }
-            if (c === 'mca') {
-                e.href = 'https://getyourresume.github.io/fresher/mca.html';
-                e.click();
-            }
-            if (c === 'mscit') {
-                e.href = 'https://getyourresume.github.io/fresher/mscit.html';
-                e.click();
-            }
-            if (c === 'mba') {
-                e.href = 'https://getyourresume.github.io/fresher/mba.html';
-                e.click();
-            }
-            if (c === 'chm') {
-                e.href = 'https://getyourresume.github.io/fresher/chm.html';
-                e.click();
-            }
-            
+function searchFunction_exp() {
+    var a = document.getElementById('myinputsearch').value.toLowerCase();
+    if (a === 'bca' || a === 'mca' || a === 'bscit' || a === 'mscit' || a === 'bba' || a === 'mba' || a === 'bhm' || a === 'chm') {
+        var b = document.getElementById('serch');
+        b.href ='expericened/' + a + '.html';
+        b.click();
+    }
+    else {
+            alert('This course is not available ');
         }
-
-function searchFunctionexp() {
-    var a = document.getElementById('myinputsearch');
-            var e = document.getElementById('click');
-            var c = a.value.toLowerCase();
-            if (c === 'bca') {
-                e.href = 'https://getyourresume.github.io/expericenced/bca.html';
-                e.click();
-            }
-            if (c === 'bscit') {
-                e.href = 'https://getyourresume.github.io/expericenced/bscit.html';
-                e.click();
-            }
-            if (c === 'bba') {
-                e.href = 'https://getyourresume.github.io/expericenced/bba.html';
-                e.click();
-            }
-            if (c === 'bhm') {
-                e.href = 'https://getyourresume.github.io/expericenced/bhm.html';
-                e.click();
-            }
-            if (c === 'mca') {
-                e.href = 'https://getyourresume.github.io/expericenced/mca.html';
-                e.click();
-            }
-            if (c === 'mscit') {
-                e.href = 'https://getyourresume.github.io/expericenced/mscit.html';
-                e.click();
-            }
-            if (c === 'mba') {
-                e.href = 'https://getyourresume.github.io/expericenced/mba.html';
-                e.click();
-            }
-            if (c === 'chm') {
-                e.href = 'https://getyourresume.github.io/expericenced/chm.html';
-                e.click();
-            }
-           
+}
+function searchFunction_frh() {
+    var a = document.getElementById('myinputsearch').value.toLowerCase();
+    if (a === 'bca' || a === 'mca' || a === 'bscit' || a === 'mscit' || a === 'bba' || a === 'mba' || a === 'bhm' || a === 'chm') {
+        var b = document.getElementById('serch');
+        b.href ='fresher/' + a + '.html';
+        b.click();
+    }
+    else {
+            alert('This course is not available ');
+        }
 }
