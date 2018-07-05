df2e5234bbfcde64869eab83439a38febc22db6f
getyourresume <rawat123pranjal111@gmail.com>
2018-07-05 15:32:49 +0530

    a

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ function searchFunction_exp() {
             alert('This course is not available ');
         }
 }
+
 function searchFunction_frh() {
     var a = document.getElementById('myinputsearch').value.toLowerCase();
     if (a === 'bca' || a === 'mca' || a === 'bscit' || a === 'mscit' || a === 'bba' || a === 'mba' || a === 'bhm' || a === 'chm') {
@@ -25,16 +26,4 @@ function searchFunction_frh() {
             alert('This course is not available ');
         }
 }
-function handleKeyPress(e){
- var key=e.keyCode || e.which;
-  if (key==13){
-     searchFunction_frh();
-  }
-}
 
-function handleKeyPress1(e){
- var key=e.keyCode || e.which;
-  if (key==13){
-     searhFunction_exp();
-  }
-}
